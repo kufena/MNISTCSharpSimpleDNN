8f45c65d5fd0fa6a50a16ba655f04919ce197809
Andrew Douglas <potterboy67@outlook.com>
2019-05-06 15:28:47 +0100

    Add project files.

---
+++ MNISTCSharpSimpleDNN/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MNISTCSharpSimpleDNN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
