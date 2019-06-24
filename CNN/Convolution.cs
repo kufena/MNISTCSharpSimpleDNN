fd6150ff3fec275438bd1eaedda15423e084a6bf
Andrew Douglas <potterboy67@outlook.com>
2019-06-24 19:42:10 +0100

    Added

---
+++ CNN/Convolution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace CNN
+{
+    public class Convolution
+    {
+
+        Matrix<double> convolution;
+
+        public Convolution(int rows, int cols)
+        {
+            convolution = Matrix<double>.Build.Dense();
+        }
+    }
+}
